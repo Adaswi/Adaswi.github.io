26a564973fb6a61788f83e4829ba2d5c2a06a95e
Adaswi <adamino2002@gmail.com>
2024-03-11 09:29:38 +0100

    Caching

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'nazwa';
 let filesToCache = [
     '/',
     '/css/styles.css',
-    '/images/logo.jpg',
+    '/images/logo.png',
     '/js/main.js'
 ];
 self.addEventListener('install', function (evt) {
